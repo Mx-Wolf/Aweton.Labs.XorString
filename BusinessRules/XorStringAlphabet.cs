a69a980973f7f6bd2a19770cb9c7ebf787007369
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-04-06 15:46:49 +0300

    describes info on configuration

--- BusinessRules/XorStringAlphabet.cs
+++ BusinessRules/XorStringAlphabet.cs
@@ -2,7 +2,7 @@ namespace Aweton.Labs.XorString.BusinessRules;
 
 internal class XorStringAlphabet
 {
-  public string Alphabet { get; set; } = null!;
+  public string Alphabet { get; set; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(),.;:\\/";
   public int OutputLength { get; set; } = 27;
   public string Role { get; set; } = null!;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # Настройки
 
-Требуется ключ MiceLicenseKey.
+Требуются ключи:
+
+* MiceLicenseKey
+* Alphabet:Role
+
+Необязательные ключи:
+
+* Alphabet:Alphabet
+* Alphabet:OutputLength
+
+Дополнительный ключ:
+
+* Encoding:Encoding (по умолчанию utf-8)
